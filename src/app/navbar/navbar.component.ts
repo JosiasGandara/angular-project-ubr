19ce41f5b2e2bf11ef43e901a1ac2bc7dea3c281
Josias Gandara <jgandara@lbsplus.mx>
2024-03-21 11:16:06 -0700

    proyecto final listo

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
 	@Input() windowTitle : string = "";
+	showLeftArrow = false;
+
+	constructor(
+		private router : Router
+	) {}
+
+	ngOnInit() {
+		this.showLeftArrow = this.router.url !== '/historialesClinicos';
+	}
 
 }
